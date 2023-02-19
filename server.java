cd31ca38f2810ec6858e2c3e4f97d6e8957c77d4
Abhishek Savaliya <114229865+abhishek1savaliya@users.noreply.github.com>
2023-02-19 21:26:46 +0530

    Update server.java

--- server.java
+++ server.java
@@ -12,7 +12,6 @@ public class server{
 
         Arrays.sort(arr);
 
-        // __________________________________________________________
         OutputStream os = s.getOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(os);
         oos.writeObject(arr);
